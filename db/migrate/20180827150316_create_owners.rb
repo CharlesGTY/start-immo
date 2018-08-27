964e389afd0b6dce24863d5e4d1e3c6c317fe8d6
Julien Mottet <mottet.julien123@gmail.com>
2018-08-27 17:03:47 +0200

    create model and controller for owners

---
+++ db/migrate/20180827150316_create_owners.rb
@@ -0,0 +1,19 @@
+class CreateOwners < ActiveRecord::Migration[5.2]
+  def change
+    create_table :owners do |t|
+      t.string :last_name
+      t.string :first_name
+      t.string :email
+      t.string :phone_number
+      t.string :address
+      t.date :birth_date
+      t.string :birth_place
+      t.string :company_name
+      t.string :siret_number
+      t.string :rcs_number
+      t.string :hq_address
+
+      t.timestamps
+    end
+  end
+end
