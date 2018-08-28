33f7eaf0c14e826092fd8125ee3686750145c631
Mateo Ceron <mateo-ceron@hotmail.fr>
2018-08-28 18:08:02 +0200

    search-bar

--- app/models/house.rb
+++ app/models/house.rb
@@ -3,4 +3,14 @@ class House < ApplicationRecord
   belongs_to :user
   monetize :price_cents
   mount_uploader :photo, PhotoUploader
+
+  include PgSearch
+    pg_search_scope :global_search,
+      against: [ :address ],
+      associated_against: {
+        owner: [ :first_name, :last_name ]
+      },
+      using: {
+        tsearch: { prefix: true }
+      }
 end
