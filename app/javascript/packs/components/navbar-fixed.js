8fdcdb929f10f0e140c8778d6d71e803ceff1d4a
Mateo Ceron <mateo-ceron@hotmail.fr>
2018-08-29 16:00:57 +0200

    navbar

--- app/javascript/packs/components/navbar-fixed.js
+++ app/javascript/packs/components/navbar-fixed.js
@@ -6,12 +6,14 @@ var navbar = document.getElementById("navbar-fixed");
 
 // Get the offset position of the navbar
 var sticky = navbar.getBoundingClientRect().top;
-console.log(sticky)
+var width = navbar.clientWidth;
+console.log(width)
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function navFixed() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    navbar.style.width = `${width}px`;
   } else {
     navbar.classList.remove("sticky");
   }
