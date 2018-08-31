6f7f8c860fe513e72565a8f5b11b824d51e56d8a
Mateo Ceron <mateo-ceron@hotmail.fr>
2018-08-31 13:08:08 +0200

    geocoder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,13 @@ import "bootstrap";
 import "../components/navbar-fixed.js";
 import "../components/tooltip";
 
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibWF0ZW9kZGIiLCJhIjoiY2psaHYyYmt6MWhqejNwbmoyM2FpZ2U1bCJ9.f3u6KplzJtqa26utv-hwRw'
+}).addTo(mymap);
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
