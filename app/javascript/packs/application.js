46ee18381e7e6735cf1dd7380950186ecc1e2c28
Mateo Ceron <mateo-ceron@hotmail.fr>
2018-09-06 13:04:23 +0200

    js polooooooolowwwwww

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,11 @@
 import "bootstrap";
 import "../components/navbar-fixed.js";
 import {initMap} from "../components/map.js";
+import {loading} from "../components/loading.js"
 import "../components/tooltip";
 import {displayAlert} from "../components/alter";
 
 initMap();
+loading();
 global.displayAlert = displayAlert;
 
